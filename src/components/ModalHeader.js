1fcd7f8db7d8d00603987785c4d5add1f65528bf
5maciek <smaciek1@op.pl>
2020-05-04 11:57:57 +0200

    add modal header

---
+++ src/components/ModalHeader.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { modalContext } from "./ModalContext";
+
+function ModalHeader({title}) {
+    const { handleToggle } = useContext(modalContext);
+
+    function closeModal() {
+        handleToggle(false);
+    }
+
+    return (
+        <header>
+            <p>{title}</p>
+            <button onClick={closeModal}>close</button>
+        </header>
+    );
+}
+
+export default ModalHeader;
